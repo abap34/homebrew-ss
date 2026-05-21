792202ee8dc2ff0b504e462ab9fe3a221bec408e
abap34 <abap0002@gmail.com>
2026-05-21 16:55:47 +0900

    ss v0.1.0

---
+++ Formula/ss.rb
@@ -0,0 +1,56 @@
+class Ss < Formula
+  desc "Slide description language and CLI"
+  homepage "https://github.com/abap34/ss"
+  url "https://github.com/abap34/ss/releases/download/v0.1.0/ss-0.1.0.tar.gz"
+  sha256 "316fdd3c378d43fc0c8a9dbf0c20c5bfd2651e60f428ea20537c0a68b4ac808f"
+  license "Apache-2.0"
+
+  depends_on "zig" => :build
+  depends_on "pkgconf" => :build
+  depends_on "cairo"
+  depends_on "imagemagick"
+  depends_on "librsvg"
+  depends_on "pango"
+  depends_on "poppler"
+  depends_on "qpdf"
+
+  resource "md4c" do
+    url "https://codeload.github.com/mity/md4c/tar.gz/472c417005c2c71b8617de4f7b8d6b30411d78f4"
+    sha256 "ecbd85292465df929839897e314d809b5c8b267e20c4e5e24d51a1602d16d99a"
+  end
+
+  def install
+    mkdir_p buildpath/"third_party"
+    resource("md4c").stage buildpath/"third_party/md4c"
+
+    system "zig", "build",
+      "-Doptimize=ReleaseSafe",
+      "-Dversion=#{version}",
+      "-Dcommit=homebrew",
+      "install",
+      "--prefix", prefix
+  end
+
+  def caveats
+    <<~EOS
+      The VS Code extension does not bundle ss. Keep #{opt_bin}/ss on PATH or
+      set the extension's ss.cli.path setting to #{opt_bin}/ss.
+
+      LaTeX math rendering requires an external TeX distribution such as
+      MacTeX or BasicTeX.
+    EOS
+  end
+
+  test do
+    (testpath/"slide.ss").write <<~EOS
+      import std:themes/default
+
+      page home
+      title_page("Homebrew", "ss formula smoke", "v#{version}")
+      end
+    EOS
+
+    assert_match version.to_s, shell_output("#{bin}/ss --version")
+    system bin/"ss", "check", testpath/"slide.ss"
+  end
+end
